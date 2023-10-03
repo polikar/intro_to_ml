2d13063a2af4987f9943c21190cf9d8f004a2a26
Robi Polikar <polikar@rowan.edu>
2023-10-03 13:49:08 -0400

    update

--- panda_example.py
+++ panda_example.py
@@ -21,4 +21,13 @@ print(df.dtypes)
 print("\n Summary statistics of the Dataframe:")
 print(df.describe())
 
+mean_sepal_length = df["Sepal_Length"].mean()
+median_sepal_length = df["Sepal_Length"].median()
+std_petal_length = df["Sepal_Length"].std()
 
+grouped_mean=df.groupby("Class").mean()
+grouped_stats=df.groupby("Class").agg(['mean', 'std', 'min', 'max'])
+
+class_count = df["Class"].value_counts()
+missing_values = df.isnull().sum()
+df.drop_duplicates(inplace=True)
